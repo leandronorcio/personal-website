e8ee6e35e6ce5141ec03591a81d2db4b4118c521
Leandro Norcio <onsongnorcio@gmail.com>
2023-10-22 23:38:40 +0800

    Refactor the button components to allow them to accept HTML button attributes.

--- app/components/ui/button-primary.tsx
+++ app/components/ui/button-primary.tsx
@@ -1,16 +1,17 @@
-export function ButtonPrimary({ children }: { children: React.ReactNode }) {
+export function ButtonPrimary({
+  children,
+  className,
+  ...rest
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+}) {
   return (
-    <button className="group relative overflow-hidden rounded-xl bg-foreground px-3 py-2 text-sm font-bold text-background transition-all active:scale-95 sm:px-4 sm:py-3 sm:text-base">
+    <button
+      className="group relative overflow-hidden rounded-xl bg-foreground px-3 py-2 text-sm font-bold text-background transition-all active:scale-95 disabled:cursor-not-allowed disabled:opacity-70 sm:px-4 sm:py-3 sm:text-base"
+      {...rest}
+    >
       {children}
       <div className="absolute left-0 top-0 z-10 h-full w-full origin-right scale-x-0 rounded-xl bg-secondary/20 transition-transform duration-500 group-hover:origin-left group-hover:scale-x-100"></div>
     </button>
   );
 }
-
-/**
- * 11/x Personal website
-
-Since the buttons are still boring, I added this sliding  hover effect using only Tailwind.
-
-It's simply an absolute div inside the button with the same border radius that scales up its width from left to right when hovered, then scales down on mouse leave.
- */
--- app/components/ui/button-secondary.tsx
+++ app/components/ui/button-secondary.tsx
@@ -1,6 +1,15 @@
-export function ButtonSecondary({ children }: { children: React.ReactNode }) {
+export function ButtonSecondary({
+  children,
+  className,
+  ...rest
+}: React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  children: React.ReactNode;
+}) {
   return (
-    <button className="group relative overflow-hidden rounded-xl bg-muted/40 px-3 py-2 text-sm font-bold text-foreground transition-all active:scale-95 sm:px-4 sm:py-3 sm:text-base">
+    <button
+      className="group relative overflow-hidden rounded-xl bg-muted/40 px-3 py-2 text-sm font-bold text-foreground transition-all active:scale-95 sm:px-4 sm:py-3 sm:text-base"
+      {...rest}
+    >
       {children}
       <div className="absolute left-0 top-0 z-10 h-full w-full origin-right scale-x-0 rounded-xl bg-secondary/20 transition-transform duration-500 group-hover:origin-left group-hover:scale-x-100"></div>
     </button>
