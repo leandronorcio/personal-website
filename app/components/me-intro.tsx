abed424cc79782b0636fda2c1d0f79d207112756
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-07 00:12:14 +0800

    Cented the hero section.

--- app/components/me-intro.tsx
+++ app/components/me-intro.tsx
@@ -4,7 +4,7 @@ import { ButtonPrimary } from './ui/button-primary';
 export default function MeIntro() {
   return (
     <div className="flex flex-col gap-5 sm:flex-row">
-      <div className="flex flex-col  items-start justify-center">
+      <div className="flex w-full flex-col items-center justify-center">
         <h1 className="font-sans text-2xl text-card-foreground">
           Hi <span className="inline-block animate-wave">👋🏻</span> I'm Leandro
           Norcio
