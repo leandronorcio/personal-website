3ff3ab5502a3d26ba4144e39f8eb840dc8fdec6d
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-07 00:00:36 +0800

    Enlarge the font size of <Badge>.

--- app/components/ui/badge.tsx
+++ app/components/ui/badge.tsx
@@ -1,6 +1,6 @@
 export function Badge({ children }: { children: React.ReactNode }) {
   return (
-    <span className="inline-flex h-6 items-center justify-center rounded bg-muted px-2 py-1 text-muted-foreground opacity-90">
+    <span className="inline-flex h-6 items-center justify-center rounded bg-muted px-2 py-1 text-lg text-muted-foreground opacity-90">
       {children}
     </span>
   );
