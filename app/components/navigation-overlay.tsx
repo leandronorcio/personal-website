252745c283e2e1c6b9215cb982254763ee3b826a
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-07 00:00:18 +0800

    Added my memoji on the navigation bar.

--- app/components/navigation-overlay.tsx
+++ app/components/navigation-overlay.tsx
@@ -1,9 +1,15 @@
-import { motion, useMotionValue, useTransform } from 'framer-motion';
+import {
+  AnimatePresence,
+  motion,
+  useMotionValue,
+  useTransform,
+} from 'framer-motion';
 import { cn } from '~/lib/cn';
 import { DarkModeSwitch } from './dark-mode-switch';
 import { Menu } from './menu';
 import { useMenu } from '~/hooks/useMenu';
 import { useEffect } from 'react';
+import memoji from '~/photos/memoji.png';
 
 export function NavigationOverlay() {
   const { isOpen, toggle } = useMenu();
@@ -23,33 +29,56 @@ export function NavigationOverlay() {
 
   return (
     <>
-      <div className="fixed top-0 z-30 flex w-full items-center justify-end gap-3 border-b border-border bg-card/30 p-4 backdrop-blur sm:gap-4">
-        <DarkModeSwitch />
-        <button
-          className={cn(
-            'relative z-40 h-[56px] w-[56px] rounded-full transition-all hover:scale-105',
-            !isOpen ? 'bg-primary' : 'border-2 border-primary',
-          )}
-          onClick={toggle}
-          aria-label="Toggle navigation"
-        >
-          <div
-            className={cn(
-              'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 rounded-xl transition-all',
-              isOpen ? '-translate-y-1/2 rotate-45' : '-translate-y-[150%]',
-              !isOpen ? 'bg-primary-foreground' : 'bg-destructive',
+      <div className="fixed top-0 z-30 flex w-full items-center justify-between  border-b border-border bg-card/30 p-3 backdrop-blur sm:p-4">
+        <div className="h-[48px] w-[48px] rounded-full sm:h-[56px] sm:w-[56px]">
+          <img src={memoji} alt="My memoji" className="h-full w-full" />
+        </div>
+        <div className="flex gap-2 sm:gap-3">
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial={{ x: -20, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: -20, opacity: 0 }}
+                className="grid place-items-center"
+              >
+                <DarkModeSwitch />
+              </motion.div>
             )}
-          ></div>
-          <div
+          </AnimatePresence>
+          <button
             className={cn(
-              'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 rounded-xl bg-primary-foreground transition-all',
-              isOpen ? '-translate-y-1/2 -rotate-45' : 'translate-y-[100%]',
-              !isOpen ? 'bg-primary-foreground' : 'bg-destructive',
+              'relative z-40 h-[48px] w-[48px] rounded-full border-2 border-muted transition-all hover:scale-105 sm:h-[56px] sm:w-[56px]',
+              !isOpen ? 'border-[1px] bg-card' : 'border-muted',
             )}
-          ></div>
-        </button>
+            onClick={toggle}
+            aria-label="Toggle navigation"
+          >
+            <div
+              className={cn(
+                'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
+                isOpen ? '-translate-y-1/2 rotate-45' : '-translate-y-[250%]',
+                !isOpen ? 'bg-foreground' : 'bg-destructive',
+              )}
+            ></div>
+            <div
+              className={cn(
+                'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
+                isOpen && '-translate-y-1/2 rotate-45',
+                !isOpen ? 'bg-foreground' : 'bg-destructive',
+              )}
+            ></div>
+            <div
+              className={cn(
+                'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
+                isOpen ? '-translate-y-1/2 -rotate-45' : 'translate-y-[250%]',
+                !isOpen ? 'bg-foreground' : 'bg-destructive',
+              )}
+            ></div>
+          </button>
+        </div>
       </div>
-      <motion.div
+      {/* <motion.div
         className="fixed left-0 top-0 z-20 h-screen w-screen bg-primary-accent"
         transition={springConfig}
         animate={{
@@ -73,7 +102,7 @@ export function NavigationOverlay() {
             clipPathX.get() + 2
           }% ${clipPathY.get()}%)`,
         }}
-      ></motion.div>
+      ></motion.div> */}
       <motion.div
         className="fixed left-0 top-0 z-20 flex h-screen w-screen items-center justify-center bg-card"
         transition={springConfig}
