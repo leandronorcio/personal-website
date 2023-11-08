9e98977258db46654d662b915a9f8d9ffb430c7d
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-09 01:55:50 +0800

    The navbar's content now has a max-width.

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -15,45 +15,47 @@ export function Navbar() {
   };
 
   return (
-    <div className="fixed top-0 z-30 flex w-full items-center justify-between  border-b border-border bg-card/30 p-3 backdrop-blur sm:p-4">
-      <div className="flex items-center gap-3">
-        <img
-          src={memoji}
-          alt="My memoji"
-          className="h-[48px] w-[48px] rounded-full sm:h-[56px] sm:w-[56px]"
-        />
-        <h1 className="text-3xl font-bold">{headers[pathname]}</h1>
+    <div className="fixed top-0 z-30 flex w-full justify-center border-b border-border bg-card/30 p-3 backdrop-blur sm:p-4">
+      <div className="flex w-full max-w-2xl items-center justify-between">
+        <div className="flex items-center gap-3">
+          <img
+            src={memoji}
+            alt="My memoji"
+            className="h-[48px] w-[48px] rounded-full sm:h-[56px] sm:w-[56px]"
+          />
+          <h1 className="text-3xl font-bold">{headers[pathname]}</h1>
+        </div>
+        <button
+          className={cn(
+            'relative z-40 h-[48px] w-[48px] rounded-full border-2 border-muted transition-all hover:scale-105 sm:h-[56px] sm:w-[56px]',
+            !isOpen ? 'border-[1px] bg-card' : 'border-muted',
+          )}
+          onClick={toggle}
+          aria-label="Toggle navigation"
+        >
+          <div
+            className={cn(
+              'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
+              isOpen ? '-translate-y-1/2 rotate-45' : '-translate-y-[250%]',
+              !isOpen ? 'bg-foreground' : 'bg-destructive',
+            )}
+          ></div>
+          <div
+            className={cn(
+              'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
+              isOpen && '-translate-y-1/2 rotate-45',
+              !isOpen ? 'bg-foreground' : 'bg-destructive',
+            )}
+          ></div>
+          <div
+            className={cn(
+              'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
+              isOpen ? '-translate-y-1/2 -rotate-45' : 'translate-y-[250%]',
+              !isOpen ? 'bg-foreground' : 'bg-destructive',
+            )}
+          ></div>
+        </button>
       </div>
-      <button
-        className={cn(
-          'relative z-40 h-[48px] w-[48px] rounded-full border-2 border-muted transition-all hover:scale-105 sm:h-[56px] sm:w-[56px]',
-          !isOpen ? 'border-[1px] bg-card' : 'border-muted',
-        )}
-        onClick={toggle}
-        aria-label="Toggle navigation"
-      >
-        <div
-          className={cn(
-            'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
-            isOpen ? '-translate-y-1/2 rotate-45' : '-translate-y-[250%]',
-            !isOpen ? 'bg-foreground' : 'bg-destructive',
-          )}
-        ></div>
-        <div
-          className={cn(
-            'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
-            isOpen && '-translate-y-1/2 rotate-45',
-            !isOpen ? 'bg-foreground' : 'bg-destructive',
-          )}
-        ></div>
-        <div
-          className={cn(
-            'absolute left-1/2 top-1/2 h-[3px] w-6 -translate-x-1/2 transition-all',
-            isOpen ? '-translate-y-1/2 -rotate-45' : 'translate-y-[250%]',
-            !isOpen ? 'bg-foreground' : 'bg-destructive',
-          )}
-        ></div>
-      </button>
     </div>
   );
 }
