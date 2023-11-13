91953d2cc4a26798fbfc508dace9a6dbb30c7342
Leandro Norcio <onsongnorcio@gmail.com>
2023-11-14 01:12:21 +0800

    Adjusted colors.

--- app/components/ui/a.tsx
+++ app/components/ui/a.tsx
@@ -8,7 +8,7 @@ export function A({
     <span className="group relative z-0 inline-block cursor-pointer">
       <a {...rest}>{children}</a>
       <span className="absolute bottom-[1px] block h-[1px] w-full bg-foreground"></span>
-      <span className="absolute bottom-[1px] block h-[1px] w-full origin-right scale-x-0 bg-primary-accent transition-transform duration-500 group-hover:origin-left group-hover:scale-x-100"></span>
+      <span className="absolute bottom-[1px] block h-[1px] w-full origin-right scale-x-0 bg-destructive transition-transform duration-500 group-hover:origin-left group-hover:scale-x-100"></span>
     </span>
   );
 }
